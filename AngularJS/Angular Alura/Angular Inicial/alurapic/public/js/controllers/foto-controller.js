15c25760fb64d8bb7d8ac4d9279495b5d82e1b3f
Tarcísio <tarcisiolima@cers.com.br>
2017-02-13 01:32:34 -0300

    Aula 8

--- AngularJS/Angular Alura/Angular Inicial/alurapic/public/js/controllers/foto-controller.js
+++ AngularJS/Angular Alura/Angular Inicial/alurapic/public/js/controllers/foto-controller.js
@@ -1,22 +1,41 @@
-angular.module('alurapic').controller('FotoController', function($scope, $http) {
+angular.module('alurapic').controller('FotoController', function($scope, $http, $routeParams) {
     
     $scope.foto = {};
     
     $scope.mensagem = '';
     
+    if ($routeParams.fotoId) {
+        $http.get('v1/fotos/' + $routeParams.fotoId).success(function(foto) {
+            $scope.foto = foto;
+        }).error(function(erro) {
+            console.log(erro);
+        });
+    }
+    
     $scope.subimeter = function () {
         
         if ($scope.formulario.$valid) {
-            
-            $http.post('v1/fotos', $scope.foto).success(function() {
+            if ($routeParams.fotoId) {
+                $http.put('v1/fotos/' + $routeParams.fotoId, $scope.foto).success(function(foto) {
+                    $scope.foto = foto;
+                    $scope.mensagem = 'Imagem alterada com sucesso!';
+                }).error(function(erro) {
+                    console.log(erro);
+                    $scope.mensagem = 'Imagem não alerada, por favor tente mais tarde!';
+                });
                 
-                $scope.foto = {};
-                $scope.mensagem = 'Imagem Cadastrada com Sucesso!';
+            } else {
                 
-            }).error(function() {
-                $scope.mensagem = 'Imagem não cadastrada, por favor tente mais tarde!';
-            });
-            
+                $http.post('v1/fotos', $scope.foto).success(function() {
+
+                    $scope.foto = {};
+                    $scope.mensagem = 'Imagem Cadastrada com Sucesso!';
+
+                }).error(function() {
+                    $scope.mensagem = 'Imagem não cadastrada, por favor tente mais tarde!';
+                });
+                
+            }
         }
     };    
 });
--- AngularJS/Angular Alura/Angular Inicial/alurapic/public/js/controllers/fotos-controller.js
+++ AngularJS/Angular Alura/Angular Inicial/alurapic/public/js/controllers/fotos-controller.js
@@ -2,11 +2,25 @@ angular.module('alurapic').controller('FotosController', function($scope, $http)
 
     $scope.fotos = [];
     $scope.filtro = '';
+    
+    $scope.mensagem = '';
 
     $http.get('v1/fotos').success(function(fotos) {
       $scope.fotos = fotos;
     }).error(function(error) {
       console.log(error);
     });
+    
+    $scope.remover = function (foto) {
+        $http.delete('v1/fotos/' + foto._id).success(function() {
+            var indexFoto = $scope.fotos.indexOf(foto);
+            $scope.fotos.splice(indexFoto, 1);
+            $scope.mensagem = 'Imagem ' + foto.titulo + " foi deletada com sucesso!";
+            console.log($scope.mensagem);
+        }).error(function(erro) {
+            console.log(erro);
+            $scope.mensagem = 'Não foi possível deletar a imagem ' + foto.titulo + '!';
+        })
+    }
 
 });
