73af203d64a5dcb95ee7d1699402ccee2f05acab
Tarcisio <tarcisio.fls@hotmail.com>
2017-04-06 23:11:09 -0300

    Aula 3

--- JavaScript/introducao-javascript/js/principal.js
+++ JavaScript/introducao-javascript/js/principal.js
@@ -1,27 +1,39 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var peso = document.querySelector("#primeiro").querySelector(".info-peso").textContent;
+//var peso = document.querySelector("#primeiro").querySelector(".info-peso").textContent;
 
-var altura = document.querySelector("#primeiro").querySelector(".info-altura").textContent;
+//var altura = document.querySelector("#primeiro").querySelector(".info-altura").textContent;
 
-var imc = peso / (altura * altura);
+//var imc = peso / (altura * altura);
 
-var alturaValida = true;
-var pesoValido = true;
+var pacientes = document.querySelectorAll(".paciente");
 
-if (peso <= 0 || peso >= 1000) {
-    console.log("Peso invalído!");
-    pesoValido = false;
-    document.querySelector("#primeiro").querySelector(".info-imc").textContent = "Peso invalído!";
-}
+for (var i = 0; i < pacientes.length; i++) {
+    
+    var peso = pacientes[i].querySelector(".info-peso").textContent;
+    var altura = pacientes[i].querySelector(".info-altura").textContent;
+    
+    var imc = peso / (altura * altura);
+    
+    var alturaValida = true;
+    var pesoValido = true;
 
-if (altura <= 0 || altura >= 3.00) {
-    console.log("Altura invalída!");
-    alturaValida = false;
-    document.querySelector("#primeiro").querySelector(".info-imc").textContent = "Altura invalída!";
-}
+    if (peso <= 0 || peso >= 1000) {
+        console.log("Peso invalído!");
+        pesoValido = false;
+        pacientes[i].querySelector(".info-imc").textContent = "Peso invalído!";
+        pacientes[i].classList.add("paciente-invalido");
+    }
 
-if (pesoValido && alturaValida) {
-    document.querySelector("#primeiro").querySelector(".info-imc").textContent = imc;
+    if (altura <= 0 || altura >= 3.00) {
+        console.log("Altura invalída!");
+        alturaValida = false;
+        pacientes[i].querySelector(".info-imc").textContent = "Altura invalída!";
+        pacientes[i].classList.add("paciente-invalido");
+    }
+
+    if (pesoValido && alturaValida) {
+        pacientes[i].querySelector(".info-imc").textContent = imc.toFixed(2);
+    }
 }
