cc76bfcdf41f5a54a484ff8eeecc8161990b6b4d
Tarcisio <tarcisio.fls@hotmail.com>
2017-05-06 14:15:04 -0300

    Inicio curso Regex

--- JavaScript/introducao-javascript/js/remover-paciente.js
+++
@@ -1,8 +0,0 @@
-document.querySelector("table").addEventListener("dblclick", function(event) {
-    event.target.parentNode.classList.add("fadeOut");
-    
-    setTimeout(function(){
-        event.target.parentNode.remove();    
-    }, 500)
-    
-});
