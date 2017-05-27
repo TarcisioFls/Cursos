f7f3f9fd9c7577ddd3a20d820c9ead00db59e54c
Tarcisio <tarcisio.fls@hotmail.com>
2017-05-27 01:11:52 -0300

    Aula 2

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -1,3 +1,15 @@
 var quantFrase =$(".frase").text().split(" ").length;
 
 $("#quant").text(quantFrase);
+
+var campo = $(".campo-digitacao");
+
+campo.on("input", function() {
+    
+    var qtdcaracteres = campo.val().length;
+    $("#contador-caracteres").text(qtdcaracteres);
+    
+    var qtdPalavras = campo.val().split(/\s+/).length - 1;
+    $("#contador-palavras").text(qtdPalavras);
+    
+});
